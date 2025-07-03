99499fa3d653ea425c4d6cf56d2512e26ca9752f
Jose Gonzalez <jose.a.gonzalez321@gmail.com>
2025-07-03 18:03:10 -0400

    End of section 4

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,13 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
-public class MembersController(AppDbContext context) : ControllerBase
+
+public class MembersController(AppDbContext context) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
@@ -16,6 +16,7 @@ public class MembersController(AppDbContext context) : ControllerBase
         return members;
     }
 
+    [Authorize]
     [HttpGet("{id}")] // api/members/bob-id
     public async Task<ActionResult<AppUser>> GetMember(string id)
     {
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +16,21 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var tokenKey = builder.Configuration["TokenKey"]
+        ?? throw new InvalidOperationException("TokenKey key not found in Program.cs");
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
@@ -24,6 +44,9 @@ app.UseCors(policy =>
               "chrome-extension://amknoiejhlmhancpahfcfcfhllgkpbld"
           );
 });
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
