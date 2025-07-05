216ccc2381e09ce6e3d966c5fb5f90566edb19ab
Jose Gonzalez <jose.a.gonzalez321@gmail.com>
2025-07-05 14:30:40 -0400

    End of section 6

---
+++ client/src/core/services/init-service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { AccountService } from './account-service';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InitService {
+  private accountService = inject(AccountService);
+
+  init(): Observable<null> {
+    const user = localStorage.getItem('user');
+    if (user) {
+      this.accountService.currentUser.set(JSON.parse(user));
+    }
+
+    return of(null); 
+  }
+}
